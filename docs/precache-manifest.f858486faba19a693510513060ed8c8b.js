bd6c7cc7ef0dfa5e928cbfb8b6429b8455143293
lrstnd <louisrostand@gmail.com>
2018-12-14 11:55:20 +0100

    add build files

---
+++ docs/precache-manifest.f858486faba19a693510513060ed8c8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fae323cef288875611bc7336134fc55d",
+    "url": "/static/media/pony.fae323ce.png"
+  },
+  {
+    "revision": "0cf26ce107d91910d88f3ac03791e6db",
+    "url": "/static/media/door.0cf26ce1.png"
+  },
+  {
+    "revision": "ea4906b2137c13efc68426840d63c424",
+    "url": "/static/media/domokun.ea4906b2.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1540f7b490f2410a519c",
+    "url": "/static/js/main.1540f7b4.chunk.js"
+  },
+  {
+    "revision": "122b0e79727fe2008435",
+    "url": "/static/js/1.122b0e79.chunk.js"
+  },
+  {
+    "revision": "1540f7b490f2410a519c",
+    "url": "/static/css/main.ac37281b.chunk.css"
+  },
+  {
+    "revision": "122b0e79727fe2008435",
+    "url": "/static/css/1.c2cdd166.chunk.css"
+  },
+  {
+    "revision": "e7538a2e21ceaee95e0e5b6e6f1aaa54",
+    "url": "/index.html"
+  }
+];
